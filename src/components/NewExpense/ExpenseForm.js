0f00a217a18a778831870605e1158e65e393d5b2
LinQuixote <leein9404@naver.com>
2023-05-10 15:04:57 +0900

    도움받아 해결

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import "./ExpenseForm.css";
 
@@ -6,6 +6,7 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [callbackValue, setCallbackValue] = useState(0);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -33,45 +34,66 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            onChange={titleChangeHandler}
-            value={enteredTitle}
-          ></input>
+  const changeExpenseForm = (event) => {
+    setCallbackValue(Number(event.target.value));
+    console.log(Boolean(event.target.value), callbackValue);
+  };
+  const Aaaa = () => {
+    return callbackValue === 0 ? (
+      <button className="" type="button" value={1} onClick={changeExpenseForm}>
+        Add New Expense
+      </button>
+    ) : (
+      <div>
+        {console.log("????")}
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              onChange={titleChangeHandler}
+              value={enteredTitle}
+            ></input>
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={amountChangeHandler}
+              value={enteredAmount}
+            ></input>
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2023-5-1"
+              max="2023-12-31"
+              onChange={dateChangeHandler}
+              value={enteredDate}
+            ></input>
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            onChange={amountChangeHandler}
-            value={enteredAmount}
-          ></input>
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2023-5-1"
-            max="2023-12-31"
-            onChange={dateChangeHandler}
-            value={enteredDate}
-          ></input>
+        <div className="new-expense__actions">
+          <button
+            className=""
+            type="button"
+            value={0}
+            onClick={changeExpenseForm}
+          >
+            Cancel
+          </button>
+          <button className="" type="submit">
+            Add Expense
+          </button>
         </div>
       </div>
-      <div className="new-expense__actions">
-        <button className="" type="submit">
-          Add Expense
-        </button>
-      </div>
-    </form>
-  );
+    );
+  };
+
+  return <form onSubmit={submitHandler}>{Aaaa()}</form>;
 };
 
 export default ExpenseForm;
