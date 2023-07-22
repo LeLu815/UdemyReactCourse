319bcd87ca0314897fc58ca15113e2503c4914f6
LinQuixote <leein9404@naver.com>
2023-07-22 15:42:42 +0900

    230722 연습프로젝트 끝

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,18 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CardContext from "../../store/cart-context";
 import classes from "./HeaderCartBtn.module.css";
 
 const HeaderCartBtn = (props) => {
+  const [btnIsHeigthlighted, setBtnIsHeigthlighted] = useState(false);
   const cartCTX = useContext(CardContext);
+  const { items } = cartCTX;
 
-  const numberOfCartItem = cartCTX.items.reduce((curNumber, item) => {
+  const numberOfCartItem = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHeigthlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHeigthlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHeigthlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span>
         <CartIcon className={classes.icon} />
       </span>
