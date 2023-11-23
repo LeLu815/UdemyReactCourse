b581a4afd838b8f796b6da129d32d379835e8b25
LinQuixote <leein9404@naver.com>
2023-11-23 22:49:50 +0900

    231123 Route, Link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import BasicForm from "./components/BasicForm";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import HomePage from "./pages/Home";
+// import Product from "./pages/Product";
+import RootLayout from "./pages/Root";
+import HomePage from "./pages/Home";
+import Product from "./pages/Product";
+import ErrorPage from "./pages/Error";
+import { Children } from "react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Product /> },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="app">
-      <BasicForm />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Product.js
@@ -0,0 +1,5 @@
+function Product() {
+  return <h1>The Product Page</h1>;
+}
+
+export default Product;
